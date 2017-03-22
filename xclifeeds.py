950abc2068a15a34c18f5c7d2c2ab0a643a9272f
lcpdn <lcpdn@users.noreply.github.com>
2017-03-22 18:09:24 +0100

    Update xclifeeds.py

--- xclifeeds.py
+++ xclifeeds.py
@@ -1,14 +1,14 @@
 FEEDS = [
-  "https://www.x-cli.eu/ct/torrents/ct.googleapis.com_aviator.rss"
-  "https://www.x-cli.eu/ct/torrents/ct.googleapis.com_pilot.rss"
-  "https://www.x-cli.eu/ct/torrents/ct.googleapis.com_rocketeer.rss"
-  "https://www.x-cli.eu/ct/torrents/ct.googleapis.com_icarus.rss"
-  "https://www.x-cli.eu/ct/torrents/ct.googleapis.com_skydiver.rss"
-  "https://www.x-cli.eu/ct/torrents/ct.startssl.com.rss"
-  "https://www.x-cli.eu/ct/torrents/ctlog.wosign.com.rss"
-  "https://www.x-cli.eu/ct/torrents/ct.ws.symantec.com.rss"
-  "https://www.x-cli.eu/ct/torrents/vega.ws.symantec.com.rss"
-  "https://www.x-cli.eu/ct/torrents/ct1.digicert-ct.com_log.rss"
-  "https://www.x-cli.eu/ct/torrents/ctlog.api.venafi.com.rss"
+  "https://www.x-cli.eu/ct/torrents/ct.googleapis.com_aviator.rss",
+  "https://www.x-cli.eu/ct/torrents/ct.googleapis.com_pilot.rss",
+  "https://www.x-cli.eu/ct/torrents/ct.googleapis.com_rocketeer.rss",
+  "https://www.x-cli.eu/ct/torrents/ct.googleapis.com_icarus.rss",
+  "https://www.x-cli.eu/ct/torrents/ct.googleapis.com_skydiver.rss",
+  "https://www.x-cli.eu/ct/torrents/ct.startssl.com.rss",
+  "https://www.x-cli.eu/ct/torrents/ctlog.wosign.com.rss",
+  "https://www.x-cli.eu/ct/torrents/ct.ws.symantec.com.rss",
+  "https://www.x-cli.eu/ct/torrents/vega.ws.symantec.com.rss",
+  "https://www.x-cli.eu/ct/torrents/ct1.digicert-ct.com_log.rss",
+  "https://www.x-cli.eu/ct/torrents/ctlog.api.venafi.com.rss",
   "https://www.x-cli.eu/ct/torrents/ctserver.cnnic.cn.rss"
 	]
